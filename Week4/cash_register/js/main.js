f73085978bf4b8e5acee0ce253d2648df0fa0cf1
Arthur Bouie <arthurbouie@gmail.com>
2015-10-14 19:50:28 -0400

    fixed with comments

---
+++ Week4/cash_register/js/main.js
@@ -0,0 +1,21 @@
+// Create a Variable called total with a value of 0
+
+
+// Use onsubmit that calls the function enter
+
+
+// Create a function called enter
+// It should have a variable for the form entry that gets the value from newEntry
+// It should then change the value of entry to a number
+// It should have then assign a value to the currency variable using the currencyFormat function you'll create below.
+// It should set the innerHTML of the entries to the currency value
+// It should reassign the value of total by adding entry to total
+// It should set the innerHTML of the total to the total value using the currencyFormat function
+// It should then clear the value of newEntry
+// It should return false to close the function
+// Google ParseFloat
+
+
+// Create the currencyFormat 
+// It should take one parameter that it will store in the a currency variable and return that variable
+// Google toFixed(2)
